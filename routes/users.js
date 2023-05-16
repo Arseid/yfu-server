9afdcd1a151e6276187657e37571bd5ee41f7203
Arseid <yvesrobert.estrada@gmail.com>
2023-05-16 15:36:08 +0200

    post request now use uid and increment user_id

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,23 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const newUser = req.body;
-        const docRef = await db.collection('users').add(newUser);
-        res.status(201).json({ id: docRef.id });
+
+        // Fetch the last used user_id in the collection 'counters' if none than set it to 700000000
+        const counterDoc = await db.collection('counters').doc('user_id').get();
+        const counterData = counterDoc.data();
+        const lastUserId = counterData ? counterData["value"] : 700000000;
+
+        // Increment user_id
+        const newUserId = lastUserId + 1;
+        newUser["user_id"] = newUserId;
+
+        // Update the counter
+        await db.collection('counters').doc('user_id').set({ value: newUserId });
+
+        // Create the new user document with the same id as the auth user's uid
+        await db.collection('users').doc(newUser["id"]).set(newUser);
+
+        res.status(201).json({ id: newUser.id });
     } catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({ error: 'Failed to create user' });

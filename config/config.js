4aa80bcc0f7c080f9cf9333ead93f8539d26524e
Arseid <yvesrobert.estrada@gmail.com>
2023-05-16 11:27:44 +0200

    connection to firebase + get users

---
+++ config/config.js
@@ -0,0 +1,16 @@
+const firebase = require('firebase/compat/app');
+require('firebase/compat/firestore');
+
+const firebaseConfig  = {
+    apiKey: "AIzaSyDO1BV-DSM-aBekyWCWp3ppBSa2setwano",
+    authDomain: "yfu-database.firebaseapp.com",
+    projectId: "yfu-database",
+    storageBucket: "yfu-database.appspot.com",
+    messagingSenderId: "584129224707",
+    appId: "1:584129224707:web:31eddf69ccfc84d39a2e36"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+module.exports = db;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/config');
 
-router.get('/', (req, res) => {
-    res.json('Salut ceci est un test');
+router.get('/', async (req, res) => {
+    try {
+        const usersSnapshot = await db.collection('users').get();
+        const users = usersSnapshot.docs.map(doc => doc.data());
+        res.json(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }
 });
 
 module.exports = router;

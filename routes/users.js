048f3b765f5aecf9466dfdaac0a8762106e3a97a
Arseid <yvesrobert.estrada@gmail.com>
2023-05-19 15:14:56 +0200

    post and delete endpoints for adding or removing a cloth item from a user

--- routes/users.js
+++ routes/users.js
@@ -83,4 +83,61 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
+// Add a clothing item to the user's clothes array
+router.post('/:userId/clothes/:clothingId', async (req, res) => {
+    try {
+        const { userId, clothingId } = req.params;
+
+        const userDocRef = db.collection('users').doc(userId);
+        const userDoc = await userDocRef.get();
+
+        if (!userDoc.exists) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Check if the clothing item already exists in the user's clothes array
+        const userClothes = userDoc.data().clothes || [];
+        if (userClothes.includes(clothingId)) {
+            return res.status(400).json({ error: 'Clothing item already exists in user\'s clothes' });
+        }
+
+        // Add the clothingId to the clothes array
+        userClothes.push(clothingId);
+
+        // Update the user document with the updated clothes array
+        await userDocRef.update({ clothes: userClothes });
+
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Error adding clothing item to user:', error);
+        res.status(500).json({ error: 'Failed to add clothing item to user' });
+    }
+});
+
+// Remove a clothing item from the user's clothes array
+router.delete('/:userId/clothes/:clothingId', async (req, res) => {
+    try {
+        const { userId, clothingId } = req.params;
+
+        const userDocRef = db.collection('users').doc(userId);
+        const userDoc = await userDocRef.get();
+
+        if (!userDoc.exists) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Remove the clothingId from the clothes array
+        let userClothes = userDoc.data().clothes || [];
+        userClothes = userClothes.filter((clothing) => clothing !== clothingId);
+
+        // Update the user document with the updated clothes array
+        await userDocRef.update({ clothes: userClothes });
+
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Error removing clothing item from user:', error);
+        res.status(500).json({ error: 'Failed to remove clothing item from user' });
+    }
+});
+
 module.exports = router;
